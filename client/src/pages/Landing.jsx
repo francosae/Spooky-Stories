348d824225ec288b345bd6a4e036d6b357fd0a6c
paul <paulfranco.e@gmail.com>
2022-10-22 03:38:19 -0400

    componn

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,8 +2,67 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <>
+        <Hero />
+    </>
   )
 }
 
+function Hero(){
+    return(
+        <section class="bg-gray-900 text-white">
+        <div
+            class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+        >
+            <div class="mx-auto max-w-3xl text-center">
+            <h1
+                class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+            >
+                Understand User Flow.
+
+                <span class="sm:block"> Increase Conversion. </span>
+            </h1>
+
+            <p class="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+                tenetur fuga ducimus numquam ea!
+            </p>
+
+            <div class="mt-8 flex flex-wrap justify-center gap-4">
+                <a
+                class="group relative inline-block focus:outline-none focus:ring"
+                href="/download"
+                >
+                <span
+                    class="absolute inset-0 translate-x-0 translate-y-0 bg-yellow-300 transition-transform group-hover:translate-y-1.5 group-hover:translate-x-1.5"
+                ></span>
+
+                <span
+                    class="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest"
+                >
+                    Sign Up
+                </span>
+                </a>
+
+
+                <a
+                class="group relative inline-block focus:outline-none focus:ring"
+                href="/download"
+                >
+                <span
+                    class="absolute inset-0 translate-x-0 translate-y-0 bg-yellow-300 transition-transform group-hover:translate-y-1.5 group-hover:translate-x-1.5"
+                ></span>
+
+                <span
+                    class="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest"
+                >
+                    Read Stories
+                </span>
+                </a>
+            </div>
+            </div>
+        </div>
+        </section>
+    )
+}
 export default Landing
--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div class="grid h-[60vh] place-content-center bg-white">
+    <div class="text-center">
+        
+     <img src="https://www.freeiconspng.com/uploads/ghost-icon-12.png" alt="404" width="350" height="450" />
+        <h1
+        class="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+        >
+        👻 Boo!
+        </h1>
+
+        <p class="mt-4 text-gray-500">Spooky... we can't find that page.</p>
+        <a
+        href="#"
+        class="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
+        >
+            Run Back Home
+        </a>
+    </div>
+    </div>
   )
 }
 
